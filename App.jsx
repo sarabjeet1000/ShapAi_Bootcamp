026d2e0b6227d9de8899e002a202073140074188
sarabjeet1000 <65124969+sarabjeet1000@users.noreply.github.com>
2021-06-05 12:05:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Body from "./Body";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Body />
+      <Body />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ Body.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div className="note">
+      <h1> React.js</h1>
+      <p>
+        this is very good bootcamp for me i am so happy to learn this bootcamp
+        and shourya sir is amazing he has amazing teaching skills.
+      </p>
+    </div>
+  );
+}
+
+export default Body;
